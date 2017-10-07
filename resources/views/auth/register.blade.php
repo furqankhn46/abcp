1ada6b692ff5c2850dd95efc3e01fed7e2a39c2b
Arif Khan <arifkhan.incaendo@gmail.com>
2017-10-07 21:05:25 +0530

    Reconstruct register page using bulma.css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,74 +2,49 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+    <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+            <div class="box">
+                <h5 class="title has-text-centered has-text-primary">Register</h5>
+                <form method="POST" action="{{ route('register') }}">
+                    {{ csrf_field() }}
+                    <div class="field">
+                        <label class="label">Name</label>
+                        <div class="control">
+                            <input class="input {{ $errors->has('name') ? ' is-danger' : '' }}" type="text" value="{{ old('name') }}" id="name" name="name" required autofocus>
                         </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        @if ($errors->has('email'))
+                            <p class="help is-danger">{{ $errors->first('email') }}</p>                        
+                        @endif
+                    </div>
+                    <div class="field">
+                        <label class="label">E-mail Address: </label>
+                        <div class="control">
+                            <input class="input {{ $errors->has('email') ? ' is-danger' : '' }}" type="text" placeholder="user@example.com" value="{{ old('email') }}" id="email" name="email" required>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        @if ($errors->has('name'))
+                            <p class="help is-danger">{{ $errors->first('email') }}</p>                        
+                        @endif
+                    </div>
+                    <div class="field">
+                        <label class="label">Password: </label>
+                        <div class="control">
+                            <input class="input {{ $errors->has('password') ? ' is-danger' : '' }}" type="password" value="{{ old('password') }}" id="password" name="password" required>
                         </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                        @if ($errors->has('password'))
+                            <p class="help is-danger">{{ $errors->first('password') }}</p>                        
+                        @endif
+                    </div>
+                    <div class="field">
+                        <label class="label">Confirm Password: </label>
+                        <div class="control">
+                            <input class="input" type="password" id="password_confirmation" name="password_confirmation" required>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="control has-text-right">
+                        <button class="button  is-medium is-primary is-outlined"> Register </button>
+                    </div>
+                </from>
             </div>
         </div>
     </div>
