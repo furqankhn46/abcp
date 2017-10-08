1340f541a0e7ffda2ceb434866f82f0e1f27a6ad
Arif Khan <arifkhan.incaendo@gmail.com>
2017-10-08 08:19:41 +0530

    Bulma sass customization

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="hero is-medium is-primary is-bold">
+<section class="hero is-medium is-primary">
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
