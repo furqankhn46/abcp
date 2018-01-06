9e7d9ad102a5b598a5521120dd7c5f81dff8e03b
Arif Khan <arifkhan.incaendo@gmail.com>
2018-01-06 15:04:13 +0530

    Login Form: Bulma related layout changes

--- public/js/app.js
+++ public/js/app.js
@@ -40642,9 +40642,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-fd1771ea", Component.options)
+    hotAPI.createRecord("data-v-09cf9c19", Component.options)
   } else {
-    hotAPI.reload("data-v-fd1771ea", Component.options)
+    hotAPI.reload("data-v-09cf9c19", Component.options)
   }
   module.hot.dispose(function (data) {
     disposed = true
@@ -40819,7 +40819,7 @@ module.exports = { render: render, staticRenderFns: staticRenderFns }
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-fd1771ea", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-09cf9c19", module.exports)
   }
 }
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="columns">
-        <div class="column is-half is-offset-one-quarter">
-            <div class="box">
-                <h4 class="title has-text-centered has-text-primary">Login</h4>
-                <form method="POST" action="{{ route('login') }}">
-                    {{ csrf_field() }}                
-                    <div class="field">
-                        <label class="label">Email</label>
-                        <div class="control has-icons-left has-icons-right">
-                            <input class="input {{ $errors->has('email') ? ' is-danger' : '' }}" type="email" placeholder="user@example.com" value="{{ old('email') }}" id="email" name="email">
-                            <span class="icon is-small is-left">
-                            <i class="fa fa-envelope"></i>
-                            </span>
-                            <span class="icon is-small is-right">
-                            <i class="fa fa-warning"></i>
-                            </span>
+<section class="hero is-fullheight">
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <div class="column is-4 is-offset-4">
+                <h3 class="title has-text-grey">Login</h3>
+                <div class="box">
+                    <form method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}                
+                        <div class="field">
+                            <div class="control has-icons-left has-icons-right">
+                                <input class="input {{ $errors->has('email') ? ' is-danger' : '' }}" type="email" placeholder="user@example.com" value="{{ old('email') }}" id="email" name="email">
+                                <span class="icon is-small is-left">
+                                <i class="fa fa-envelope"></i>
+                                </span>
+                                @if ($errors->has('email')) 
+                                    <span class="icon is-small is-right">
+                                        <i class="fa fa-warning"></i>
+                                    </span>
+                                @endif
+                            </div>
+                            @if ($errors->has('email'))
+                                <p class="help is-danger">{{ $errors->first('email') }}</p>                        
+                            @endif
                         </div>
-                        @if ($errors->has('email'))
-                            <p class="help is-danger">{{ $errors->first('email') }}</p>                        
-                        @endif
-                    </div>
-                    <div class="field">
-                        <label class="label">Password</label>
-                        <p class="control has-icons-left">
-                            <input class="input {{ $errors->has('password') ? ' is-danger' : '' }}" type="password" placeholder="Password" name="password" id="password">
-                            <span class="icon is-small is-left">
-                            <i class="fa fa-lock"></i>
-                            </span>
-                        </p>
-                        @if ($errors->has('password'))
-                            <p class="help is-danger">{{ $errors->first('password') }}</p>                        
-                        @endif
-                    </div>
-                    <div class="field">
-                        <div class="control">
-                            <label class="checkbox">
-                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                            </label>
+                        <div class="field">
+                            <p class="control has-icons-left">
+                                <input class="input {{ $errors->has('password') ? ' is-danger' : '' }}" type="password" placeholder="Password" name="password" id="password">
+                                <span class="icon is-small is-left">
+                                <i class="fa fa-lock"></i>
+                                </span>
+                            </p>
+                            @if ($errors->has('password'))
+                                <p class="help is-danger">{{ $errors->first('password') }}</p>                        
+                            @endif
                         </div>
-                    </div>
-                    <div class="field is-grouped">
-                        <div class="control">
-                            <button class="button is-primary">Login</button>
+                        <div class="field">
+                            <div class="control">
+                                <label class="checkbox">
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                </label>
+                            </div>
                         </div>
-                        <div class="control">
-                            <a class="button is-link" href="{{ route('password.request') }}">
-                                Forgot Your Password?
-                            </a>
-                        </div>
-                    </div>
-                </form>
+                        <button type="submit" class="button is-info is-fullwidth">Login</button>
+                        <a class="button is-link" href="{{ route('password.request') }}">
+                            Forgot Your Password?
+                        </a>
+                    </form>
+                </div>
+                <p>
+                    <a href="../" class="has-text-grey">Sign Up</a> &nbsp;·&nbsp;
+                    <a href="../" class="has-text-grey">Forgot Password</a> &nbsp;·&nbsp;
+                    <a href="../" class="has-text-grey">Need Help?</a>
+                </p>
             </div>
         </div>
     </div>
-</div>
+</section>
 @endsection
